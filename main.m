590623c9a7444f1468aeedb9faa25e51d0513db9
DanielHunter3 <154628544+DanielHunter3@users.noreply.github.com>
2024-01-01 02:47:41 +0400

    Update main.m

--- main.m
+++ main.m
@@ -12,64 +12,64 @@
 
 @implementation Math
 
-(instancetype)initWithX:(double)x andY:(double)y {
-self = [super init];
-if (self) {
-_x = x;
-_y = y;
-}
-return self;
+-(instancetype)initWithX:(double)x andY:(double)y {
+    self = [super init];
+    if (self) {
+        _x = x;
+        _y = y;
+    }
+    return self;
 }
 
-(double)mathOperationsWithSymbol:(NSString )symbol {
-if ([symbol isEqualToString:@"+"]) return _x + _y;
-else if ([symbol isEqualToString:@"-"]) return _x - _y;
-else if ([symbol isEqualToString:@""]) return _x * _y;
-else if ([symbol isEqualToString:@"/"]) {
-if (_y == 0.0) {
-NSLog(@"The Y is 0");
-abort();
-}
-else return _x / _y;
-}
-else if ([symbol isEqualToString:@"^"]) return powf(_x, _y);
-else {
-NSLog(@"This is not math operation: %@", symbol);
-abort();
-}
+-(double)mathOperationsWithSymbol:(NSString )symbol {
+    if ([symbol isEqualToString:@"+"]) return _x + _y;
+    else if ([symbol isEqualToString:@"-"]) return _x - _y;
+    else if ([symbol isEqualToString:@""]) return _x * _y;
+    else if ([symbol isEqualToString:@"/"]) {
+        if (_y == 0.0) {
+            NSLog(@"The Y is 0");
+            abort();
+        }
+        else return _x / _y;
+    }
+    else if ([symbol isEqualToString:@"^"]) return powf(_x, _y);
+    else {
+        NSLog(@"This is not math operation: %@", symbol);
+        abort();
+    }
 }
 
 @end
 
 int main(int argc, const char * argv[]) {
-@autoreleasepool {
-double x = 0.0, y = 0.0;
-NSString *symbol;
+    @autoreleasepool {
+        double x = 0.0, y = 0.0;
+        NSString *symbol;
 
-    NSLog(@"Enter a first number: ");
-    scanf("%lf", &x);
+        NSLog(@"Enter a first number: ");
+        scanf("%lf", &x);
     
-    NSLog(@"Enter a math symbol: ");
-    char buffer[100];
-    scanf("%s", buffer);
-    symbol = [NSString stringWithUTF8String:buffer];
+        NSLog(@"Enter a math symbol: ");
+        char buffer[100];
+        scanf("%s", buffer);
+        symbol = [NSString stringWithUTF8String:buffer];
     
-    NSArray *vec = @[@"+", @"-", @"*", @"/", @"^"];
+        NSArray *vec = @[@"+", @"-", @"*", @"/", @"^"];
     
-    NSString *result = @"";
-    BOOL isBool = [vec containsObject:symbol];
+        bNSString *result = @"";
+        BOOL isBool = [vec containsObject:symbol];
     
-    if (!isBool) {
-        NSLog(@"This is not math operation: %@", symbol);
-        abort();
+        if (!isBool) {
+            NSLog(@"This is not math operation: %@", symbol);
+            abort();
+        }
+    
+        NSLog(@"Enter a second number: ");
+        scanf("%lf", &y);
+    
+        Math *math = [[Math alloc] initWithX:x andY:y];
+        NSLog(@"Result: %lf", [math mathOperationsWithSymbol:result]);
     }
-    
-    NSLog(@"Enter a second number: ");
-    scanf("%lf", &y);
-    
-    Math *math = [[Math alloc] initWithX:x andY:y];
-    NSLog(@"Result: %lf", [math mathOperationsWithSymbol:result]);
-}
 return 0;
 
 }
